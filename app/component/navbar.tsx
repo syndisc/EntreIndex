c8ffe2d8d89618f67d5ad0499f25196f347b4320
ZenX <zenxdiscord@gmail.com>
2024-03-26 15:43:23 +0700

    change home

--- app/component/navbar.tsx
+++ app/component/navbar.tsx
@@ -28,20 +28,19 @@ const Navbar = () => {
   }, [])
 
   return (
-    <div className='w-screen h-auto flex justify-between bg-secondaryLight dark:bg-secondaryDark sticky top-0'>
+    <div className='w-screen h-auto flex justify-between bg-cyan-600 sticky top-0 shadow-2xl'>
         <Link href={"/home"} className='flex '>
-            <Image width={100} height={100} src="/images/logo.png" alt="Asset not found" className='w-20 h-16'/>
+            <Image width={100} height={100} src="/images/logo2.png" alt="Asset not found" className='w-24 h-20'/>
             <div className='flex flex-col content-center justify-center flex-wrap  text-textLight dark:text-textDark'>
               <div className='font-bold text-3xl'>
               Indonesia Entrepeneurship Index
               </div>
-              <div>
+              <div className='font-bold'>
                 Welcome, {user?.first_name}
               </div>
             </div>
         </Link>
         <div className='w-auto gap-6 flex justify-around content-center flex-wrap font-bold text-lg text-textLight dark:text-textDark pr-4'>
-            <Link href={"/form"}>Form</Link>
             <Link href={"/answer"}>My Answer</Link>
             <Link href={"/data"}>Data</Link>
             <Link href={"user/profile"}>Profile</Link>
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,16 +1,49 @@
 import React from 'react'
 import Navbar from '../component/navbar'
 import CardLineChart from '../component/LineChart'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const page = () => {
   
   return (
-    <div className='h-auto w-screen bg-primaryLight dark:bg-primaryDark'>
+    <div className='h-auto w-screen bg-gradient-to-br from-cyan-200 to-blue-900'>
       <Navbar/>
       <div className='h-93.2vh w-screen flex justify-center content-center flex-wrap'>
         <div className='h-85vh w-11/12 bg-secondaryLight dark:bg-secondaryDark p-6 rounded-3xl shadow-2xl'>
-          <div className='h-full overflow-y-auto'>
-            
+          <div className='h-full y-full'>
+            {/* top part */}
+            <div className=' h-1/2 p-2  w-full '>
+              <div className='bg-cyan-600 rounded-3xl flex w-full h-full p-6 shadow-2xl'>
+                {/* top left */}
+                <div className='w-2/3'>
+                  <div className='text-5xl font-bold'>
+                    FILL A QUESTIONNAIRE
+                  </div>
+                  <div className='text-lg font-semibold text-justify'>
+                  Join us on an exhilarating adventure! Your insights matter in shaping the future of entrepreneurship. By participating in our comprehensive questionnaire on entrepreneurship, you're not just contributing your thoughts but also igniting the flame of innovation and creativity. Let's pave the way for a brighter future, where every aspiring entrepreneur can thrive. Together, let's make entrepreneurship an irresistible journey towards success and impact!
+                  </div>
+                  <Link href="/form">
+                      <button className='p-2 bg-white text-cyan-600 font-bold rounded-3xl hover:bg-yellow-500 hover:text-white'>
+                          Fill a questionnaire
+                      </button>
+                  </Link>
+                </div>
+                {/* top right */}
+                <div className='w-1/3'>
+                  <Image width={600} height={800} src="/images/map2.png" alt='not found'/>
+                </div>
+              </div>
+            </div>
+            {/* bot part */}
+            <div className=' flex w-full h-1/2 p-2 justify-between'>
+              <div className='w-47.5/100 bg-green-500 p-6 rounded-3xl shadow-2xl'>
+                BOT LEFT
+              </div>
+              <div className='w-47.5/100 bg-blue-600 p-6 rounded-3xl shadow-2xl'>
+                BOT RIGHT
+              </div>
+            </div>
           </div>
         </div>
       </div>
